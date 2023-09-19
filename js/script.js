cd661bf9e69f9dc57b2b42699ecb88b48aa1390f
Pirisinu <palazzialessandro2@gmail.com>
2023-09-19 17:40:06 +0200

    [push]

--- js/script.js
+++ js/script.js
@@ -12,7 +12,8 @@ let difCounter;
 
 const bombTot = 16;
 const bombFind = [];
-let idBomb = [];
+let idBombArray = [];
+let idBomb ;
 
 difficultyChoice.append(btnStart());
 
@@ -21,17 +22,18 @@ difficultyChoice.append(btnStart());
 /* FUNZIONI GENERALI */
 //INIT FUNCTION
 function init(){
+
   //Richiamo la select per ottenere il suo valore
   difficultySelected = document.querySelector('#select-difficulty');
 
+  //Genero gli id delle bombe
+  idBombGeneretor(bombTot);
+  console.log(idBombGeneretor(bombTot));
+
   //Vado a ciclare in base al valore della select
   for (let i = 1; i <= difficultySelected.value; i++) {
-
     //Genero i quadrati
     const square = createBox(i);
-    
-    //Vado ad creare con un randomizzatore le bombe
-    
 
     //EventListner al click
     square.addEventListener('click', function(){
@@ -49,10 +51,10 @@ function init(){
 function btnStart(){
   //creiamo il bottone in html
   const btn = document.createElement('button');
-
+  
   //ci scrivo sopra
   btn.innerHTML= 'Scegli prima la difficoltà, <br/> poi clicca qui per iniziare.';
-
+  
   //Aggiungo un evento al click per far iniziare il gioco
   btn.addEventListener('click', function(){
     //Elimina cio che c'è in precedenza
@@ -60,24 +62,30 @@ function btnStart(){
     //Inizia una nuova partita
     init();
   });
-
+  
   return btn;
 }
 
 //RANDOM NUM GENERATOR FUNCTION
 function randomNum(min, max){
- return Math.floor(Math.random() * (max - min +1) + min);
+  return Math.floor(Math.random() * (max - min +1) + min);
 }
 
-for(let i=1; i <= bombTot; i++) {
-function randomIdBomb(bombTot){
-    idBomb = Math.floor(Math.random() * bombTot);
-    idBomb.appwm
-    return idBomb;
-  }
-  console.log(randomIdBomb(idBomb));
-  }
 
+
+function idBombGeneretor(bombTot) {
+  let idBombSet  = [];
+  let count = 0;
+
+  while (count < 16) {
+    let idBomb = Math.floor(Math.random() * difficultySelected.value) + 1;
+    if (!idBombSet.includes(idBomb)) {
+      idBombSet.push(idBomb);
+      count++;
+    }
+  }
+  return idBombSet;
+}
 //ELEMENT GENERATOR FUNCTION
 function createBox(index){
   const square = document.createElement('div');
